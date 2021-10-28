80270ea31c80d6d378dcd5e2deb8dddf06786723
Jacob Co <jacob.co@factset.com>
2021-10-28 13:57:04 +0800

    Converted auth service into a class

--- backend/services/auth.service.ts
+++ backend/services/auth.service.ts
@@ -1,38 +1,40 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+dotenv.config();
 
 import { User } from '../user-defined-types';
 import { Request, Response, NextFunction } from 'express';
 
-dotenv.config();
-const secret = process.env.AUTH_SECRET || 'replace me with a secret';
+export class AuthService {
+  private static readonly secret = process.env.AUTH_SECRET || 'replace me with a secret';
 
-export function generateUserAuthToken(user: User) {
-  const data = {
-    id: user.id,
-    username: user.username,
-    date: (new Date()).getTime()
+  public static generateUserAuthToken(user:User): string {
+    const data = {
+      id: user.id,
+      username: user.username,
+      date: (new Date()).getTime()
+    }
+  
+    return jwt.sign(data, AuthService.secret, {});
   }
 
-  return jwt.sign(data, secret, {});
-}
-
-export function verifyUserToken(req: Request, res: Response, next: NextFunction) {
-  let token = req.headers.authorization;
-
-  if (typeof token !== 'undefined') {
-		token = token.slice(7, token.length);
-
-    return jwt.verify(token, secret, (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(403).send('invalid token')
-      } else {
-        return next();
-      }
-    })
-  } else {
-    console.error('No token received')
-    return res.status(403).send('no token');
+  public static verifyUserToken(req: Request, res: Response, next: NextFunction) {
+    let token = req.headers.authorization;
+  
+    if (typeof token !== 'undefined') {
+      token = token.slice(7, token.length);
+  
+      return jwt.verify(token, AuthService.secret, (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(403).send('invalid token')
+        } else {
+          return next();
+        }
+      })
+    } else {
+      console.error('No token received')
+      return res.status(403).send('no token');
+    }
   }
 }
