454794a8f88c444b7ab76a000d5180485412e660
Jacob Co <jacob.co@factset.com>
2021-11-02 13:03:15 +0800

    Added admin token verify and new toekn formulation

--- backend/services/auth.service.ts
+++ backend/services/auth.service.ts
@@ -9,13 +9,11 @@ export class AuthService {
   private static readonly secret = process.env.AUTH_SECRET || 'replace me with a secret';
 
   public static generateUserAuthToken(user:User): string {
-    const data = {
-      id: user.id,
-      username: user.username,
-      date: (new Date()).getTime()
-    }
+    const userDetails = user as any;
+    delete userDetails.password
+    userDetails.date = (new Date()).getTime()
   
-    return jwt.sign(data, AuthService.secret, {});
+    return jwt.sign(userDetails, AuthService.secret, {});
   }
 
   public static verifyUserToken(req: Request, res: Response, next: NextFunction) {
@@ -26,6 +24,27 @@ export class AuthService {
   
       return jwt.verify(token, AuthService.secret, (err, data) => {
         if (err) {
+          console.error(err);
+          return res.status(403).send('invalid token')
+        } else {
+          if (data && data.isAdmin) return next();
+          return res.status(403).send('Not an admin')
+        }
+      })
+    } else {
+      console.error('No token received')
+      return res.status(403).send('no token');
+    }
+  }
+
+  public static verifyAdminToken(req: Request, res: Response, next: NextFunction) {
+    let token = req.headers.authorization;
+  
+    if (typeof token !== 'undefined') {
+      token = token.slice(7, token.length);
+  
+      return jwt.verify(token, AuthService.secret, (err, data) => {
+        if (data.isAdmin || err) {
           console.error(err);
           return res.status(403).send('invalid token')
         } else {
