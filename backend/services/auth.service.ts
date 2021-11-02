89a708361c99bcdf46ff1face8770592f2fde20f
Jacob Co <jacob.co@factset.com>
2021-11-02 13:07:39 +0800

    Changed auth.service into exported functions

--- backend/services/auth.service.ts
+++ backend/services/auth.service.ts
@@ -5,55 +5,53 @@ dotenv.config();
 import { User } from '../user-defined-types';
 import { Request, Response, NextFunction } from 'express';
 
-export class AuthService {
-  private static readonly secret = process.env.AUTH_SECRET || 'replace me with a secret';
+const secret = process.env.AUTH_SECRET || 'replace me with a secret';
 
-  public static generateUserAuthToken(user:User): string {
-    const userDetails = user as any;
-    delete userDetails.password
-    userDetails.date = (new Date()).getTime()
-  
-    return jwt.sign(userDetails, AuthService.secret, {});
+export function generateUserAuthToken(user:User): string {
+  const userDetails = user as any;
+  delete userDetails.password
+  userDetails.date = (new Date()).getTime()
+
+  return jwt.sign(userDetails, secret, {});
+}
+
+export function verifyUserToken(req: Request, res: Response, next: NextFunction) {
+  let token = req.headers.authorization;
+
+  if (typeof token !== 'undefined') {
+    token = token.slice(7, token.length);
+
+    return jwt.verify(token, secret, (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(403).send('invalid token')
+      } else {
+        return next();
+      }
+    })
+  } else {
+    console.error('No token received')
+    return res.status(403).send('no token');
   }
+}
 
-  public static verifyUserToken(req: Request, res: Response, next: NextFunction) {
-    let token = req.headers.authorization;
-  
-    if (typeof token !== 'undefined') {
-      token = token.slice(7, token.length);
-  
-      return jwt.verify(token, AuthService.secret, (err, data) => {
-        if (err) {
-          console.error(err);
-          return res.status(403).send('invalid token')
-        } else {
-          if (data && data.isAdmin) return next();
-          return res.status(403).send('Not an admin')
-        }
-      })
-    } else {
-      console.error('No token received')
-      return res.status(403).send('no token');
-    }
-  }
+export function verifyAdminToken(req: Request, res: Response, next: NextFunction) {
+  let token = req.headers.authorization;
 
-  public static verifyAdminToken(req: Request, res: Response, next: NextFunction) {
-    let token = req.headers.authorization;
-  
-    if (typeof token !== 'undefined') {
-      token = token.slice(7, token.length);
-  
-      return jwt.verify(token, AuthService.secret, (err, data) => {
-        if (data.isAdmin || err) {
-          console.error(err);
-          return res.status(403).send('invalid token')
-        } else {
-          return next();
-        }
-      })
-    } else {
-      console.error('No token received')
-      return res.status(403).send('no token');
-    }
+  if (typeof token !== 'undefined') {
+    token = token.slice(7, token.length);
+
+    return jwt.verify(token, secret, (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(403).send('invalid token')
+      } else {
+        if (data && data.isAdmin) return next();
+        return res.status(403).send('Not an admin token');
+      }
+    })
+  } else {
+    console.error('No token received')
+    return res.status(403).send('no token');
   }
 }
