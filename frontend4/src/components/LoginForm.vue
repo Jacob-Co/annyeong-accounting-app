e248be49b419f97aae75c49c01a710327c066101
Jacob Co <jacob.co@factset.com>
2021-11-07 22:18:57 +0800

    Added frontend

---
+++ frontend4/src/components/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form>
+    <div>
+      <label for="usernameInput">Username</label>
+      <input type="text" id="usernameInput"/>
+    </div>
+    <div>
+      <label for="passwordInput">Password</label>
+      <input type="password" id="passwordInput"/>
+    </div>
+    <button class="btn btn-primary">Login</button>
+  </form>
+</template>
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+
+  @Options({})
+  export default class LoginForm extends Vue {}
+</script>
