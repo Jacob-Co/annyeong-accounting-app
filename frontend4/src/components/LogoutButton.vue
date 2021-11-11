65b8186ecdeb9f5865b544efb37036f9ffb0c95e
Jacob Co <jacob.co@factset.com>
2021-11-11 13:36:13 +0800

    Added logout button

---
+++ frontend4/src/components/LogoutButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn btn-secondary" @click="logout">Logout</button>
+</template>
+
+<script lang="ts">
+  import { Vue, Options } from 'vue-class-component';
+  import router from '../router/index';
+
+  @Options({})
+  export default class LogoutButton extends Vue {
+    public logout() {
+      window.localStorage.removeItem('annyeongToken');
+      router.push('Login');
+    }
+  }
+</script>
