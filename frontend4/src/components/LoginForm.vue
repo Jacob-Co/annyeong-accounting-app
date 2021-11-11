fefc7bab82a78e3cdd097be71af1489c75a49f04
Jacob Co <jacob.co@factset.com>
2021-11-11 13:21:04 +0800

    Added Login Form

--- frontend4/src/components/LoginForm.vue
+++ frontend4/src/components/LoginForm.vue
@@ -1,20 +1,27 @@
 <template>
-  <form>
+  <div>
     <div>
       <label for="usernameInput">Username</label>
-      <input type="text" id="usernameInput"/>
+      <input type="text" id="usernameInput" v-model="username"/>
     </div>
     <div>
       <label for="passwordInput">Password</label>
-      <input type="password" id="passwordInput"/>
+      <input type="password" id="passwordInput" v-model="password"/>
     </div>
-    <button class="btn btn-primary">Login</button>
-  </form>
+    <button class="btn btn-primary" @click="loginClick">Login</button>
+  </div>
 </template>
 
 <script lang="ts">
   import { Options, Vue } from 'vue-class-component';
-
+  import { Login } from '../functions/login-form.function';
   @Options({})
-  export default class LoginForm extends Vue {}
+  export default class LoginForm extends Vue {
+    public username = '';
+    public password = '';
+
+    public loginClick() {
+      Login.impl({ username: this.username, password: this.password});
+    }
+  }
 </script>
