81a82e123db8dfc0e493e812d882f5581f448976
Jacob Co <jacob.co@factset.com>
2021-11-26 23:03:07 +0800

    Added get process from PORT

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ import { distributorsRouter } from './routers/distributor.router';
 import { stocksRouter } from './routers/stocks.router';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectToDatabse()
   .then(() => {
