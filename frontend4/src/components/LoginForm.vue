212941d403ef316f1d4aef167a54cfbc0db73b00
Jacob Co <jacob.co@factset.com>
2021-11-27 00:10:11 +0800

    Added loading during login

--- frontend4/src/components/LoginForm.vue
+++ frontend4/src/components/LoginForm.vue
@@ -1,15 +1,21 @@
 <template>
-  <form>
-    <div>
-      <label for="usernameInput">Username</label>
-      <input type="text" id="usernameInput" v-model="username"/>
+  <div>
+    <div class="spinner-border text-primary" role="status" v-show="isLoading">
+      <span class="visually-hidden">Loading...</span>
     </div>
-    <div>
-      <label for="passwordInput">Password</label>
-      <input type="password" id="passwordInput" v-model="password"/>
-    </div>
-    <button class="btn btn-primary" @click="loginClick">Login</button>
-  </form>
+
+    <form v-show="!isLoading">
+      <div>
+        <label for="usernameInput">Username</label>
+        <input type="text" id="usernameInput" v-model="username"/>
+      </div>
+      <div>
+        <label for="passwordInput">Password</label>
+        <input type="password" id="passwordInput" v-model="password"/>
+      </div>
+      <button class="btn btn-primary" @click="loginClick">Login</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,10 +26,13 @@
   export default class LoginForm extends Vue {
     public username = '';
     public password = '';
+    public isLoading = false;
 
-    public loginClick(event: Event) {
-      event.preventDefault(); 
-      Login.impl({ username: this.username, password: this.password});
+    public async loginClick(event: Event) {
+      event.preventDefault();
+      this.isLoading = true;
+      await Login.impl({ username: this.username, password: this.password});
+      this.isLoading = false;
     }
   }
 </script>
