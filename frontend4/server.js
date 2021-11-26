04493f2582ee940aefb256d81dab58aed9756981
Jacob Co <jacob.co@factset.com>
2021-11-27 01:19:14 +0800

    Removed static.json

--- frontend4/server.js
+++ frontend4/server.js
@@ -14,6 +14,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+console.log("Server.js is running", path.join(__dirname, 'dist'))
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 app.listen(port);
